a5918a74add2801bf7cb89405852ca1111f0c5c6
wolfpack94 <benedea@students.wwu.edu>
2017-02-14 16:44:27 -0800

    Updated dataValidation.py with capabilities of grabbing the max line length and using that to validate other rows.

--- dataValidation.py
+++ dataValidation.py
@@ -1,8 +1,12 @@
 import sys, os
 
-def pull_row_len(line):
-    split_line = line.split(',')
-    return len(split_line)
+def pull_max_row_len(infile):
+    max_len = 0
+    for line in infile:
+        split_line = line.split(',')
+        if len(split_line) > max_len:
+            max_len = len(split_line)
+    return max_len
 
 
 def validate_row(line,columns):
@@ -34,15 +38,18 @@ def parse_file(infile=None):
         print "File " + input_file + " cannot be opened."
         exit()
 
-    cols = pull_row_len(input_file.readline())
+    cols = pull_max_row_len(input_file)
+    print cols
+    input_file.seek(0)
     num_occurrences = 0
     if cols > 0:
         for line in input_file:
             split_line = line.split(',')
             if not validate_row(split_line,cols):
                 num_occurrences += 1
-                output_file.write("Line found: \n"+line+"\n")
+                output_file.write("Line found: \n"+"Length of line: "+str(len(split_line))+" Supposed length: "+str(cols)+"\n"+line+"\n")
         output_file.write("Number of records with differences: "+str(num_occurrences))
+    return num_occurrences
 
 
 def main():
@@ -50,10 +57,14 @@ def main():
 
     if len(sys.argv) > 1:
         input_file = sys.argv[1]
+    else:
+        print "Usage: python dataValidation.py [input file]"
+        exit()
 
-    parse_file(input_file)
+    occurrences = parse_file(input_file)
 
     print "Analysis Complete"
+    print "Records found: " + str(occurrences)
 
 if __name__ == "__main__":
     main()
